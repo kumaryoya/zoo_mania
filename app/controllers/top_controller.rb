d6aedc132334de9e1dc65dd65358d9945deb4bd6
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-07-08 00:41:14 +0900

    プライバシーポリシーを実装しました

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,3 +1,5 @@
 class TopController < ApplicationController
   def index; end
+
+  def privacy_policy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "top#index"
+  get 'privacy_policy', to: 'top#privacy_policy'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
