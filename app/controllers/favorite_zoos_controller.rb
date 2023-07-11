cc4d8a29985749c39e7458fa27da59853cb07a56
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-07-11 23:04:32 +0900

    ヘッダーを修正しました

--- app/controllers/favorite_zoos_controller.rb
+++ app/controllers/favorite_zoos_controller.rb
@@ -1,32 +1,26 @@
 class FavoriteZoosController < ApplicationController
 
-  def new
-    @user = User.find(params[:user_id])
-    @favorite_zoo = @user.favorite_zoos
-    @favorite_zoos = current_user.favorite_zoos.order(rank: :asc)
-  end
-
   def create
-    @user = User.find(params[:user_id])
+    @user = User.find(current_user.id)
     @favorite_zoo = @user.favorite_zoos.build(favorite_zoo_params)
     @favorite_zoos = current_user.favorite_zoos.order(rank: :asc)
     if @favorite_zoo.save
-      redirect_to new_user_favorite_zoo_path, notice: "お気に入りの動物園を追加しました。"
+      redirect_to profile_path, notice: "お気に入りの動物園を追加しました。"
     else
-      flash.now[:danger] = "失敗"
-      render :new, status: :unprocessable_entity
+      flash.now[:danger] = "作成できなかったゾゥ"
+      render template: "profiles/show", status: :unprocessable_entity
     end
   end
 
   def destroy
-    @user = User.find(params[:user_id])
+    @user = User.find(current_user.id)
     @favorite_zoo = @user.favorite_zoos.find(params[:id])
     @favorite_zoos = current_user.favorite_zoos.order(rank: :asc)
     if @favorite_zoo.destroy
-      redirect_to new_user_favorite_zoo_path, notice: "お気に入りの動物園を削除しました。"
+      redirect_to profile_path, notice: "お気に入りの動物園を削除しました。"
     else
-      flash.now[:danger] = "削除に失敗しました。"
-      render :index, status: :unprocessable_entity
+      flash.now[:danger] = "削除に失敗したゾゥ"
+      render template: "profiles/show", status: :unprocessable_entity
     end
   end
 
