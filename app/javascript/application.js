f5bd59c04b9ab257a3c999a592cedeb2c01e3de9
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-07-17 12:01:53 +0900

    twitter共有機能を実装しました

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-//= require jquery
-//= require rails-ujs
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@fortawesome/fontawesome-free"
