26c51754962d61f207d268510918151ce0b42f5a
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-07-17 14:02:20 +0900

    ビューを修正しました

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,17 +2,13 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @post.likes.create(user: current_user)
-    respond_to do |format|
-      format.turbo_stream
-    end
+    respond_to(&:turbo_stream)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @like = @post.likes.find_by(user: current_user)
     @like.destroy
-    respond_to do |format|
-      format.turbo_stream
-    end
+    respond_to(&:turbo_stream)
   end
 end
