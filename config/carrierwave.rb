6d57f618dabd9e05f3b3729f01a673adc97486c0
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-07-18 14:55:33 +0900

    fix

--- config/carrierwave.rb
+++ config/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'S3のバケット名'
+  config.fog_directory  = 'zoomania'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
