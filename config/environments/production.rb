58f7bcf9371709a866fbad4654b984e32917fe94
りょうや <127639508+kumaryoya@users.noreply.github.com>
2023-07-20 12:54:24 +0900

    Revert "Fix"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: Settings.default_url_options[:host] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default = { charset: 'utf-8' }
+  config.action_mailer.default :charset => 'utf-8'
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
     address: 'smtp.gmail.com',
