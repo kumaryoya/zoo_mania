da9360836c77125a4a29b3fd95cc67c176dbbc80
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-07-20 15:51:24 +0900

    ogp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,6 @@ module ApplicationHelper
 
   def default_meta_tags
     {
-      site: 'ZooMania',
-      title: "ZooMania",
       reverse: true,
       charset: 'utf-8',
       description: 'みんなで動物園に行こう！',
@@ -15,9 +13,6 @@ module ApplicationHelper
       canonical: request.original_url,
       separator: '|',
       og: {
-        site_name: :site,
-        title: :title,
-        description: :description,
         type: 'website',
         url: request.original_url,
         image: image_url('ogp.png'),
