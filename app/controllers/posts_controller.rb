0bc652798ae0e47d2bb358bbc8dbfd5362be1b8c
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-07-22 02:17:39 +0900

    投稿詳細ページを実装しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def edit; end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     end
   end
   resources :password_resets, only: %i[new create edit update]
-  resources :posts, only: %i[index new create edit update destroy] do
+  resources :posts, only: %i[index new create show edit update destroy] do
     resource :likes, only: [:create, :destroy]
     collection do
       get 'ranking', to: 'posts#ranking'
