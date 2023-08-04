1ab217c36050509974af0a5d1f7c8a5550dd9002
Okuma Ryoya <kumaryoya05210521@yahoo.co.jp>
2023-08-05 01:48:49 +0900

    pwa

---
+++ public/service_worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'weather_app_cache';
+const urlsToCache = [
+  '/',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
