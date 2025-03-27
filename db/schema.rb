94355cb94ddb5b166ff07bc8c988e2770bf31800
kumaryoya <kumaryoya05210521@gmail.com>
2025-03-27 22:41:55 +0900

    セットアップ

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_15_135722) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "favorite_zoos", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "zoo_id"
+    t.bigint "user_id"
+    t.bigint "zoo_id"
     t.integer "rank"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,8 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_135722) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "post_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
@@ -33,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_135722) do
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.string "image", null: false
-    t.integer "user_id", null: false
-    t.integer "zoo_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "zoo_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
